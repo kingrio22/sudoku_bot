1b36c1ccec7cdf6e109aef373766fcb08182f360
King Rio <max.graetz@t-online.de>
2020-04-12 19:59:39 +0200

    updated path to assets

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,11 @@ async function bootstrap() {
   const port = configService.get('PORT');
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.useStaticAssets(join(process.cwd(), '../sudoku/src/public'));
+  app.useStaticAssets(join(__dirname, '..', '/src/public'));
   app.setBaseViewsDir(join(__dirname, '..', '/src/sudoku/frontend'));
   app.setViewEngine('hbs');
 
   await app.listen(port);
   console.log(chalk.greenBright('app running on port ', port));
-  console.log('process cwd: ', join(process.cwd(), '../sudoku/src/public'));
-  console.log('dirname', join(__dirname, '..', '/src/sudoku/frontend'));
 }
 bootstrap();
